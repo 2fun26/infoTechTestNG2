8f66ed2b79136de3d373a55785617c54c3be39e4
OMER <omertufan26@yandex.com>
2022-05-20 19:29:23 +0100

    5

--- src/test/java/com/infoTech/utilities/TestBase.java
+++ src/test/java/com/infoTech/utilities/TestBase.java
@@ -1,6 +1,37 @@
 package com.infoTech.utilities;
 
-public class TestBase {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
+import java.util.concurrent.TimeUnit;
 
+public abstract class TestBase {
+
+    // abstract class' lardan nesne olusturulamaz.
+    //diger class' lardan miras almak icin bu sekilde olusturduk.
+
+    //private => sadece ayni class
+    //default => ayni paket
+    //protected => ayni paket ve child class' larda(farkli paketteki)
+    //public => her yerde.
+
+    protected WebDriver driver;
+    @BeforeClass
+    public void setUp(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @AfterClass
+    public void tearDown(){
+
+     //   driver.close();
+    }
 }
