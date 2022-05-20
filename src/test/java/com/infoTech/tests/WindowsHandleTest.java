6741dd5d29bcba05106069df26863a9fcb06ba86
OMER <omertufan26@yandex.com>
2022-05-20 19:58:46 +0100

    7

--- src/test/java/com/infoTech/tests/WindowsHandleTest.java
+++ src/test/java/com/infoTech/tests/WindowsHandleTest.java
@@ -1,7 +1,11 @@
 package com.infoTech.tests;
 
 import com.infoTech.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.util.Set;
 
 public class WindowsHandleTest extends TestBase {
 
@@ -10,7 +14,18 @@ public class WindowsHandleTest extends TestBase {
 
     driver.get("https://the-internet.herokuapp.com/windows");
 
+    // sayfanin handle' ini aldik
     String handle = driver.getWindowHandle();
         System.out.println(handle);
+
+        driver.findElement(By.partialLinkText("Click Here")).click();
+
+        //Tum sayfalarin handle' inin aliyoruz.
+
+        Set<String> tumElementler = driver.getWindowHandles();
+        for(String w : tumElementler){
+
+            System.out.println(w);
+        }
     }
 }
