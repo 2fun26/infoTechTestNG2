ec797e1fb2a74b24c62c904909fa56c2acc70c0c
OMER <omertufan26@yandex.com>
2022-05-30 20:30:36 +0100

    uc

---
+++ src/test/java/com/infoTech/excelautomation/WriteExcel.java
@@ -0,0 +1,34 @@
+package com.infoTech.excelautomation;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class WriteExcel {
+    @Test
+    public void test() throws IOException {
+        String dosyaYolu = ".\\src\\test\\resources\\ULKELER.xlsx";
+        //Java'da bir dosyayı açmak için bu kullanılır.
+        // Parametre bölümüne açmak istediğimiz dosyanın adresi (dosya yolu,path) yazılır
+        FileInputStream fileInputStream = new FileInputStream(dosyaYolu);
+        // Workbook (excel) dosyasını okumaya başladık.
+        Workbook workbook = WorkbookFactory.create(fileInputStream);
+        // Dosya üzerinde yazma işlemi gerçekleştireceğimiz için buraya
+        // FileOutputStream class'ından bir tane nesne oluşturmamız gerekiyor.
+        FileOutputStream fileOutputStream = new FileOutputStream(dosyaYolu);
+        // ÜLKELER  BAŞKENTLER  NEYIMESHUR  NÜFUS
+        // 0. indexteki satırın, 3. indexteki hücresine NÜFUS hücresi ekleyelim
+        workbook.getSheetAt(0).getRow(0).createCell(3).setCellValue("NÜFUS");
+        workbook.getSheetAt(0).getRow(1).createCell(3).setCellValue("80000000");
+        // yaptığımız değişiklikleri excel dosyasına aktarır, veriyi yazar ve kaydeder.
+        workbook.write(fileOutputStream);
+        fileOutputStream.close();
+        workbook.close();
+        fileInputStream.close();
+    }
+
+}
